6a380e19fa164789f73c634cab2cec82504a79a6
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-23 14:37:40 +0100

    Add favicon

--- routes/critters.js
+++ routes/critters.js
@@ -10,7 +10,6 @@
 // });
 
 
-
 const router = require("express").Router();
 const chalk = require("chalk");
 const axios = require("axios");
@@ -21,47 +20,46 @@ const User = require("../models/User.model");
 //My own middleware
 const isLoggedIn = require("../middleware/isLoggedIn")
 
-router.get("/critters", isLoggedIn ,(req, res, next) => {
-  const loggedUser = req.session.loggedUser
-  res.render("critters");
-});
+// router.get("/critters", isLoggedIn ,(req, res, next) => {
+//   const loggedUser = req.session.loggedUser
+//   res.render("critters");
+// });
 
 router.get("/critters", isLoggedIn, async (req, res) => {
   try {
-    const axiosCall = await axios(
-      `http://acnhapi.com/v1/fish/`
-    );
-    const crittersInfo = axiosCall.data.data.results; //esto es un array
+    const axiosCall = await axios(`http://acnhapi.com/v1/fish/`)
+    const crittersInfo = axiosCall.data.results;
+    console.log (crittersInfo)
     res.render("./critters.hbs", { crittersInfo });
   } catch (err) {
     console.log(chalk.bgRed(err));
   }
 });
 
-router.post("/create/:id", async (req, res) => {
-  const axiosCall = await axios(
-    `http://acnhapi.com/v1/fish/`
-  );
+// router.post("/create/:id", async (req, res) => {
+//   const axiosCall = await axios(
+//     `http://acnhapi.com/v1/fish/`
+//   );
 
-  const infoFromCritter = axiosCall.data.data.results;
+//   const infoFromCritter = axiosCall.data.data.results;
 
-  const dataToUpload = {
-    name: infoFromCritter[0].name,
-    location: infoFromCritter[0].location,
-    rarity: infoFromCritter[0].rarity,
-    catchPhrase: infoFromCritter[0].catchPhrase,
-    image: infoFromCritter[0].image
-   };
+//   const dataToUpload = {
+//     name: infoFromCritter[0].name,
+//     location: infoFromCritter[0].location,
+//     rarity: infoFromCritter[0].rarity,
+//     catchPhrase: infoFromCritter[0].catchPhrase,
+//     image: infoFromCritter[0].image
+//    };
 
-  const justCreatedCritter = await Critter.create(dataToUpload);
+//   const justCreatedCritter = await Critter.create(dataToUpload);
 
-  await User.findByIdAndUpdate(
-    req.session.loggedUser._id,
-    { $push: { critters: justCreatedCritter._id } },
-    { new: true }
-  );
+//   await User.findByIdAndUpdate(
+//     req.session.loggedUser._id,
+//     { $push: { critters: justCreatedCritter._id } },
+//     { new: true }
+//   );
 
-  res.redirect('/')
-});
+//   res.redirect('/')
+// });
 
 module.exports = router;
